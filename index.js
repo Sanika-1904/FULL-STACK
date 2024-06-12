b7ac4c65c5aa116c5e3263438a33da303864ccd0
Sanika-1904 <149898553+Sanika-1904@users.noreply.github.com>
2024-06-11 22:48:03 -0700

    index.js

--- index.js
+++ index.js
@@ -13,11 +13,8 @@ const path=require("path")
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
-app.get('/',(req,res)=>{
-   res.send("HELLO");
-})
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
    res.render('home')
 })
 
